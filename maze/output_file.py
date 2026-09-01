ff559ab9fa41bc4a29b890161235a84a278fb416
Svetlana Koreneva <corenewa.s@gmail.com>
2026-09-01 11:13:16 +0200

    add find path

--- maze/output_file.py
+++ maze/output_file.py
@@ -1,7 +1,7 @@
 from maze.models import Cell
 from config.models import Config
 
-def generate_output_file(grid: list[list[Cell]], config: Config):
+def generate_output_file(grid: list[list[Cell]], config: Config, path):
 	with open(config.output_file, 'w') as output_file:
 		for y in range(len(grid)):
 			for x in range(len(grid[y])):
@@ -10,5 +10,6 @@ def generate_output_file(grid: list[list[Cell]], config: Config):
 				output_file.write(f"{output:x}")
 			output_file.write('\n')
 		output_file.write('\n')
-		output_file.write(f"{config.entry.x, config.entry.y}    # entry (x,y)\n")
-		output_file.write(f"{config.exit.x, config.exit.y}    # exit (x,y)\n")
+		output_file.write(f"{config.entry.x},{config.entry.y}\n")
+		output_file.write(f"{config.exit.x},{config.exit.y}\n")
+		output_file.write(path)
--- todo.md
+++ todo.md
@@ -2,13 +2,13 @@
 
 - [ ] Add Makefile
 - [ ] Implement the configuration parser. [Svetlana - started]
-- [ ] Implement the maze and cell data model.
-- [ ] Implement maze generation.
+- [x] Implement the maze and cell data model. [Svetlana - done]
+- [ ] Implement maze generation. [Svetlana - started]
 - [ ] Add seed support.
 - [ ] Implement `PERFECT` and non-perfect maze modes.
 - [ ] Implement entry and exit handling.
-- [ ] Implement the `42` pattern.
+- [x] Implement the `42` pattern. [Svetlana - done]
 - [ ] Implement graphical rendering using MiniLibX.
-- [ ] Implement output file generation.
+- [ ] Implement output file generation. [Svetlana - started]
 - [ ] Write the README.
 - [ ] Clean up and refactor the project.
