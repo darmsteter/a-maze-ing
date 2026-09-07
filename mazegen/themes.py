82b31ed01c94abea3647c63a84670be07c436bfe
Carmen-Elena Bruma <160878690+carmenelenab@users.noreply.github.com>
2026-09-08 00:42:56 +0200

    add themes file - maze display ideas/options

---
+++ mazegen/themes.py
@@ -0,0 +1,42 @@
+
+EMOJI_THEMES = {
+    "tree_garden": {
+        "wall": lambda t: t.on_lightgreen('🌳'),
+        "path": lambda t: t.on_black('🟫'),
+        "start": lambda t: t.on_yellow('𖠋 '),
+        "exit": lambda t: t.on_white('🚪'),
+    },
+    "snowman": {
+        "wall": lambda t: t.on_lightblue('☃️ '),
+        "path": lambda t: t.on_lightblue('  '),
+        "start": lambda t: t.on_blue('⛇ '),
+        "exit": lambda t: t.on_red('❄️ '),
+    },
+    "beach": {
+        "wall": lambda t: t.on_lightyellow('🌴'),
+        "path": lambda t: t.on_brown('🏻'),
+        "start": lambda t: t.on_green('𖠋 '),
+        "exit": lambda t: t.on_red('🏆'),
+    },
+    "christmas": {
+        "wall": lambda t: t.on_lightblue('🎄'),  # 🧱
+        "path": lambda t: t.on_lightblue('⬜'),
+        "start": lambda t: t.on_blue('🎅'),
+        "exit": lambda t: t.on_white('🎁'),
+    },
+}
+
+LINE_THEMES = {
+    "classic_pink": {
+        "wall": lambda t: t.on_pink('▓▓'),
+        "path": lambda t: t.on_black('  '),
+        "start": lambda t: t.on_yellow(' S'),
+        "exit": lambda t: t.on_red(' E'),
+    },
+    "cyber_blue": {
+        "wall": lambda t: t.on_blue('▓▓'),
+        "path": lambda t: t.on_purple('▒▒'),
+        "start": lambda t: t.on_yellow(' S'),
+        "exit": lambda t: t.on_red(' Ⓔ '),
+    },
+}
