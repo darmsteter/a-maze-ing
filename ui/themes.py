8127b07cee0427a89ec8c906755c4bcfbf75e893
Carmen-Elena Bruma <160878690+carmenelenab@users.noreply.github.com>
2026-09-16 23:21:43 +0200

    fix: themes colors and patterns

--- ui/themes.py
+++ ui/themes.py
@@ -4,72 +4,93 @@ from blessed import Terminal
 
 EMOJI_THEMES = {
     "tree_garden": {
-        "wall": lambda t: t.on_green('🌳'),
-        "path": lambda t: t.on_green('🏾'),
-        "start": lambda t: t.on_yellow('🚩'),
-        "exit": lambda t: t.on_red('🏁'),
-        "solution_path": lambda t: t.on_lightgreen('🐾'),
-        "pattern_42": lambda t: t.blink_on_green('🌸')
-    },
-    "snowman": {
-        "wall": lambda t: t.on_darkblue('☃️ '),
-        "path": lambda t: t.on_darkblue('  '),
-        "start": lambda t: t.bold_black_on_blue('❄️ '),
-        "exit": lambda t: t.bold_white_on_lightblue('🗻'),
-        "solution_path": lambda t: t.on_darkblue('🧊'),
-        "pattern_42": lambda t: t.on_darkblue('🥶')
+        "wall": lambda t: t.on_darkolivegreen('🌳'),  # 🍄🌸🌻
+        "path": lambda t: t.darkolivegreen('  '),
+        "start": lambda t: t.black('🐝'),
+        "exit": lambda t: t.black('🌸'),
+        "solution_path": lambda t: t.darkolivegreen('✨'),
+        "pattern_42": lambda t: t.on_darkolivegreen('🍄')
     },
     "beach": {
-        "wall": lambda t: t.on_lightyellow('🌴'),
-        "path": lambda t: t.on_brown('🏻'),
-        "start": lambda t: t.bold_black_on_yellow('𖠋 '),
-        "exit": lambda t: t.bold_white_on_red('🏆'),
-        "solution_path": lambda t: t.on_lightgreen('🐾'),
-        "pattern_42": lambda t: t.on_orange('🧱')
-    },
-    "christmas": {
-        "wall": lambda t: t.on_lightblue('🎄'),  # 🧱
-        "path": lambda t: t.on_lightblue('⬜'),
-        "start": lambda t: t.on_blue('🎅'),
-        "exit": lambda t: t.bold_white_on_red('🎁'),
-        "solution_path": lambda t: t.on_lightgreen('🐾'),
-        "pattern_42": lambda t: t.on_orange('🧱')
+        "wall": lambda t: t.on_color_rgb(238, 214, 175)('🌴'),
+        "path": lambda t: t.on_color_rgb(238, 214, 175)('  '),
+        "start": lambda t: t.bold_black_on_yellow('🐶'),
+        "exit": lambda t: t.bold_white_on_red('🏠'),
+        "solution_path": lambda t: t.on_color_rgb(238, 214, 175)('🐾'),
+        "pattern_42": lambda t: t.on_yellow('☀️ ')
     },
     "halloween": {
-        "wall": lambda t: t.on_black('💀'),  # 🧱☠️💀⚰🦴🧟🥀🩸
+        "wall": lambda t: t.on_black('💀'),  # 🧱☠️💀⚰🦴🧟🥀🩸🔥🦇🎃🍬🎪👻🧛🏻‍♀️🔮
         "path": lambda t: t.on_black('  '),
-        "start": lambda t: t.bold_black_on_white('🏰'),
-        "exit": lambda t: t.bold_white_on_red('🧟'),
-        "solution_path": lambda t: t.on_black('🦇'),
-        "pattern_42": lambda t: t.on_black('🥀')
+        "start": lambda t: t.on_black('🎪'),
+        "exit": lambda t: t.on_black('🎃'),
+        "solution_path": lambda t: t.on_black('🍬'),
+        "pattern_42": lambda t: t.on_black('🔥')
+    },
+    "snowman": {
+        "wall": lambda t: t.on_color(153)('🥶'),  # 🧣🥕☃️ 🥶 🍫 ☕️🧊🛷🏠🐇
+        "path": lambda t: t.on_color(153)('  '),
+        "start": lambda t: t.on_color(153)('🐇'),
+        "exit": lambda t: t.on_color(153)('☃️ '),
+        "solution_path": lambda t: t.on_color(153)('🥕'),
+        "pattern_42": lambda t: t.on_color(153)('🧣')
+    },
+   
+    "christmas": {
+        "wall": lambda t: t.on_color(153)('🏠'),  # 🧱🎁🏠🎄🛷✨🫎🧸🦌
+        "path": lambda t: t.on_color(153)('  '),
+        "start": lambda t: t.on_color(153)('🎅'),
+        "exit": lambda t: t.on_color(153)('🎁'),
+        "solution_path": lambda t: t.on_color(153)('🛷'),
+        "pattern_42": lambda t: t.on_color(153)('🎄')
     },
 }
+# 🏠 🌼  
 
 LINE_THEMES = {
-    "classic_pink": {
-        "wall": lambda t: t.on_green('▒▒'),
+    "green": {
+        "wall": lambda t: t.green('██'),
         "path": lambda t: t.on_black('  '),
-        "start": lambda t: t.bold_black_on_yellow(' S'),
-        "exit": lambda t: t.bold_white_on_red(' E'),
-        "solution_path": lambda t: t.white_on_black('○'),
+        "start": lambda t: t.bold_yellow_on_black('S '),
+        "exit": lambda t: t.bold_red_on_black('E '),
+        "solution_path": lambda t: t.white_on_black('~'),
+        "pattern_42": lambda t: t.bold_yellow_on_orange('42')
+    },
+    "pink": {
+        "wall": lambda t: t.on_pink('  '),
+        "path": lambda t: t.on_magenta('  '),
+        "start": lambda t: t.bold_red_on_yellow('S '),
+        "exit": lambda t: t.bold_yellow_on_red('E '),
+        "solution_path": lambda t: t.bold_red_on_magenta('*'),
         "pattern_42": lambda t: t.blink_bold_yellow_on_purple('██')
     },
-    "cyber_blue": {
-        "wall": lambda t: t.on_blue('▓▓'),
+    "neon": {
+        "wall": lambda t: t.blue('▓▓'),
         "path": lambda t: t.on_purple('  '),
-        "start": lambda t: t.bold_black_on_yellow(' S'),
-        "exit": lambda t: t.bold_white_on_red(' E'),
-        "pattern_42": lambda t: t.on_yellow('██')
+        "start": lambda t: t.bold_black_on_yellow('S '),
+        "exit": lambda t: t.bold_white_on_red('E '),
+        "solution_path": lambda t: t.on_purple('★'),
+        "pattern_42": lambda t: t.blink_on_bright_yellow('  ')
     },
-    "box_drawing": {
-        "wall": lambda t: t.on_green('██'),
-        "path": lambda t: '  ',
-        "start": lambda t: t.bold_black_on_yellow(' S'),
-        "exit": lambda t: t.bold_white_on_red(' E'),
-        "solution_path": lambda t: t.white_on_black('○'),
-        "pattern_42": lambda t: t.on_orange('  ')
-    }
+    "blue": {
+        "wall": lambda t: t.on_darkblue('  '),
+        "path": lambda t: t.on_lightblue('  '),
+        "start": lambda t: t.bold_red_on_yellow('S '),
+        "exit": lambda t: t.bold_yellow_on_red('E '),
+        "solution_path": lambda t: t.on_lightblue('★'),
+        "pattern_42": lambda t: t.on_magenta('  ')
+    },
+    "yellow": {
+        "wall": lambda t: t.yellow('▓▓'),
+        "path": lambda t: t.on_lightyellow('  '),
+        "start": lambda t: t.bold_red_on_yellow('S '),
+        "exit": lambda t: t.bold_yellow_on_red('E '),
+        "solution_path": lambda t: t.on_lightyellow('★'),
+        "pattern_42": lambda t: t.on_red('  ')
+    },
+    
 }
+# ▒ ▒. ▓▓, █,
 
 
 def get_tile(
@@ -80,12 +101,9 @@ def get_tile(
 ) -> str:
     """Getting the grafic format from selected theme"""
     themes_dict = EMOJI_THEMES if mode == "emoji" else LINE_THEMES
-    default_theme = "tree_garden" if mode == "emoji" else "classic_pink"
+    default_theme = "tree_garden" if mode == "emoji" else "green"
 
     theme = themes_dict.get(theme_name, themes_dict[default_theme])
     tile_formatter = theme.get(tile_type, lambda t: '  ')
 
     return tile_formatter(term)
-
-# ▒ ▒. ▓▓, █,
-# 🏠 🌼 🍄
